e964fee672dee1368638737428afaeb75c710180
Alex Curran <alexcurran.va@gmail.com>
2020-09-23 14:46:55 -0500

    Added Helmet and Compression

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 
 const express = require("express");
 const socket = require("socket.io");
+const helmet = require("helmet");
+const compression = require("compression");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // Middleware
+app.use(helmet());
+app.use(compression());
 app.use(express.static("public"));
-
 
 const server = app.listen(PORT, () => console.log(`Server listening on port, ${PORT}.`));
 
@@ -16,8 +19,6 @@ const server = app.listen(PORT, () => console.log(`Server listening on port, ${P
 const io = socket(server);
 
 io.on("connection", (socket) => {
-    console.log("Made socket connection", socket.id);
-
     socket.on("chat", (data) => {
         io.sockets.emit("chat", data);
     });
