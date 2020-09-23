3c0459ea5578b451d0a425e5ab459839b8b40b1c
Alex Curran <alexcurran.va@gmail.com>
2020-09-23 15:20:54 -0500

    Updated script CDN for socket.io and commented out Helmet

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
-app.use(helmet());
+// app.use(helmet());
 app.use(compression());
 app.use(express.static("public"));
 
