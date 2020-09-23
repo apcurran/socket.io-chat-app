0b085a199902d7c1534a83f14cedb503be094d44
Alex Curran <alexcurran.va@gmail.com>
2020-09-23 14:40:58 -0500

    Functionality finished

---
+++ public/chat.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// Make connection
+const socket = io.connect("http://localhost:5000");
+
+const message = document.getElementById("message");
+const handle = document.getElementById("handle");
+const output = document.getElementById("output");
+const chatForm = document.getElementById("chat-form");
+const feedback = document.getElementById("feedback");
+
+// Emit events
+chatForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    socket.emit("chat", {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+message.addEventListener("keypress", () => {
+    socket.emit("typing", handle.value);
+})
+
+// Listen for events
+socket.on("chat", (data) => {
+    // Remove previous "***** is typing" messages
+    removeAllChildEls(feedback);
+
+    const paraEl = document.createElement("p");
+    const strongEl = document.createElement("strong");
+    const spanEl = document.createElement("span");
+
+    strongEl.textContent = `${data.handle}: `;
+    spanEl.textContent = data.message;
+
+    paraEl.append(strongEl, spanEl);
+    output.append(paraEl);
+});
+
+socket.on("typing", (data) => {
+    const paraEl = document.createElement("p");
+    const emEl = document.createElement("em");
+
+    emEl.textContent = `${data} is typing a message...`;
+
+    paraEl.append(emEl);
+
+    // Remove previous elements in feedback div
+    removeAllChildEls(feedback);
+
+    feedback.append(paraEl);
+});
+
+function removeAllChildEls(parentEl) {
+    while (parentEl.firstChild) {
+        parentEl.removeChild(parentEl.firstChild);
+    }
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require("express");
+const socket = require("socket.io");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware
+app.use(express.static("public"));
+
+
+const server = app.listen(PORT, () => console.log(`Server listening on port, ${PORT}.`));
+
+// Socket Setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection", socket.id);
+
+    socket.on("chat", (data) => {
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on("typing", (data) => {
+        socket.broadcast.emit("typing", data);
+    });
+});
